00ed954aec3b61fbc80aeb52177938e2a890861b
Samer Haija <samer.haija@gmail.com>
2020-08-23 15:23:05 -0700

    5MJIm initial commit

---
+++ lib/fivemjim_view_tool.rb
@@ -0,0 +1,8 @@
+require "fivemjim_view_tool/version"
+require "fivemjim_view_tool/renderer"
+
+
+module FiveMJImViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/fivemjim_view_tool/version.rb
@@ -0,0 +1,3 @@
+module FivemjimViewTool
+  VERSION = "0.1.0"
+end
